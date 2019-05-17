19176755de97339328ecb7f2839945515c13935a
Richard Ginzburg <richard@amphisys.se>
2019-05-17 10:34:46 +0100

    send add_to_cart events

--- analytics/app/src/main/java/com/google/firebase/quickstart/analytics/kotlin/ImageFragment.kt
+++ analytics/app/src/main/java/com/google/firebase/quickstart/analytics/kotlin/ImageFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.quickstart.analytics.R
 
 /**
@@ -30,8 +32,28 @@ class ImageFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_main, null)
         val imageView = view.findViewById<ImageView>(R.id.imageView)
         imageView.setImageResource(resId)
+        imageView.setOnClickListener(View.OnClickListener {
+            logFirebaseEvents()
+        })
 
         return view
+    }
+
+    private fun logFirebaseEvents() {
+        val addToCartBundle = Bundle()
+
+        addToCartBundle.putString(FirebaseAnalytics.Param.ITEM_ID, "123456")
+        addToCartBundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Test product")
+        addToCartBundle.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "Test category")
+        addToCartBundle.putLong(FirebaseAnalytics.Param.QUANTITY, 1L)
+        addToCartBundle.putDouble(FirebaseAnalytics.Param.PRICE, 123.0)
+        addToCartBundle.putDouble(FirebaseAnalytics.Param.VALUE, 123.0)
+        addToCartBundle.putString(FirebaseAnalytics.Param.CURRENCY, "SEK")
+
+        context?.let {
+            FirebaseAnalytics.getInstance(it).logEvent(FirebaseAnalytics.Event.ADD_TO_CART, addToCartBundle)
+            Toast.makeText(it, "Sent ${FirebaseAnalytics.Event.ADD_TO_CART}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
